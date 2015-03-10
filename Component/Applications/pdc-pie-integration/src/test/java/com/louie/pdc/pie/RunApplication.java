6845d3772fc555affada68791a35d3daa8804328
paul.b.m.lugtu <louielugtu21@gmail.com>
2015-03-10 22:26:07 +0800

    Added SFTP Adapter

---
+++ Component/Applications/pdc-pie-integration/src/test/java/com/louie/pdc/pie/RunApplication.java
@@ -0,0 +1,24 @@
+package com.louie.pdc.pie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:/config/sftp/pie-integration-context.xml"})
+public class RunApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(RunApplication.class);
+	
+	@Test
+	public void run() {
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			LOGGER.error("Error occured while sleeping.", e);
+		}
+	}
+}
